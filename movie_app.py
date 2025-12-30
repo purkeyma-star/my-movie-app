1e47eb69e76c448c5ebc2c7af4a533fc23052b66
purkeyma-star <purkeyma@gmail.com>
2025-12-30 02:39:07 -0600

    Update movie_app.py

--- movie_app.py
+++ movie_app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Plex Movie Collection", page_icon="🎬")
 
 # 1. THE URL - Paste your link between the quotes below
 # Make sure it ends in /edit or /view
-SHEET_URL = "https://docs.google.com/spreadsheets/d/YOUR_ACTUAL_ID_HERE/edit"
+SHEET_URL = "https://docs.google.com/spreadsheets/d/1-AtYz6Y6-wVls2EIuczq8g0RkEHnF0n8VAdjcpiK4dE/edit?usp=sharing"
 
 st.title("🎬 My Movie Collection Manager")
 
